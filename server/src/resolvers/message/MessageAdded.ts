c784e0a6b45e36b4314909919d71c0181f471a56
Konstantin Polyanskiy <konstantin_polyanskiy@epam.com>
2020-01-21 14:13:46 +0400

    BE - Remove commented code

--- server/src/resolvers/message/MessageAdded.ts
+++ server/src/resolvers/message/MessageAdded.ts
@@ -5,7 +5,6 @@ import { MESSAGE_ADDED, MessageAddedPayload } from './subscriptionTypes';
 
 @Resolver()
 export class MessageAddedResolver {
-  // @Authorized()
   @Subscription({topics: MESSAGE_ADDED})
   messageAdded(
     @Root() {message}: MessageAddedPayload,
